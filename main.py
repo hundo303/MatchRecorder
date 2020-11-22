00ad0b307ee0befdf35536ff9c2701d93cecace0
hundo <44696693+hundo303@users.noreply.github.com>
2020-11-22 13:53:15 +0900

    打席の投球内容が取れた

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import crawling
 
 #  (year=2020, opening_date='01-01', ending_date='12-31')みたいに渡してほしい
-crawling.crawling(year=2020, opening_date='06-30', ending_date='6-30')
+crawling.crawling(year=2020, opening_date='11-02', ending_date='11-14')
--- scraping.py
+++ scraping.py
@@ -13,3 +13,31 @@ def exists_next_page(html_dir):
     return os.path.exists(html_dir)
 
 
+#  1球ごとの投球内容のリストを内包した、1打席の投球内容のリストを返す
+#  1球ごとの投球内容は[打席での球数, 試合での球数, 球種, 球速, 結果]
+def take_ball_list(html):
+    one_pitch_list = []
+
+    soup = BeautifulSoup(html, 'html.parser')
+    tr_list = soup.select('#pitchesDetail > section:nth-child(2) > table:nth-child(3) > tbody > tr')
+    for tr in tr_list:
+        td_list = tr.select('td')
+        pitch_number_in_at_bat = td_list[0].span.get_text()
+        pitch_number_in_game = td_list[1].get_text()
+        type_of_pitch = td_list[2].get_text()
+        pitch_speed = td_list[3].get_text()
+        pitch_result = re.sub(r'\s', '', td_list[4].get_text())
+
+        one_pitch_list.append([pitch_number_in_at_bat,
+                               pitch_number_in_game,
+                               type_of_pitch,
+                               pitch_speed,
+                               pitch_result])
+
+    return one_pitch_list
+
+
+with open(r'D:/prog/MatchRecorder/HTML/2020061901/0110100.html', encoding='utf-8') as f:
+    result = take_ball_list(f)
+    for data in result:
+        print(data)
