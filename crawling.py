9c76b2f39b9c1ae13fa270bcd61a41da7ce9f019
hundo <44696693+hundo303@users.noreply.github.com>
2020-12-01 22:20:27 +0900

    crawlingにstatsとplayer_listを追加

--- crawling.py
+++ crawling.py
@@ -9,7 +9,7 @@ import time
 #  ゲームごとにdirを作ってその中にゲームのHTMLを保存する
 #  dirが既に存在すると取ってこない
 #  (year=2020, opening_date='01-01', ending_date='12-31')みたいに渡してほしい
-def crawling_season(year, opening_date, ending_date):
+def crawling(year, opening_date, ending_date):
     opening_month = int(opening_date.split('-')[0])
     opening_day = int(opening_date.split('-')[1])
     ending_month = int(ending_date.split('-')[0])
@@ -18,6 +18,36 @@ def crawling_season(year, opening_date, ending_date):
     date_url_list = make_date_url_list(year, opening_month, opening_day, ending_month, ending_day)
 
     fetch_day(date_url_list)
+
+
+def crawling_game_stats(year, opening_date='01-01', ending_date='12-31'):
+    opening_month = int(opening_date.split('-')[0])
+    opening_day = int(opening_date.split('-')[1])
+    ending_month = int(ending_date.split('-')[0])
+    ending_day = int(ending_date.split('-')[1])
+
+    date_url_list = make_date_url_list(year, opening_month, opening_day, ending_month, ending_day)
+    fetch_day_stats(date_url_list)
+
+
+def fetch_day_stats(date_url_list):
+    for date_url in date_url_list:
+        for game_no in range(1, 7):
+            stats_url = date_url + str(game_no).zfill(2) + r'/stats'
+            file_dir = os.getcwd() + fr'\HTML_game_stats\{stats_url[-16:-6]}.html'
+
+            if os.path.exists(file_dir):
+                continue
+
+            time.sleep(1)
+            result = requests.get(stats_url)
+
+            if result.status_code == 404:
+                break
+
+            with open(file_dir, 'w', encoding='utf-8') as f:
+                f.write(result.text)
+                print('Done')
 
 
 #  URLの日付の部分まで作る
@@ -72,15 +102,15 @@ def fetch_game_html(url):
         return Exception('Error:status_code404')
 
     #  日付+ゲーム番号(例:2020061901)のディレクトリ名
-    game_no_str = re.search('\d{10}', url).group()
-    gameDir = os.getcwd() + f'\\HTML\\{game_no_str}'
+    game_no_str = re.search(r'\d{10}', url).group()
+    gameDir = os.getcwd() + r'\HTML\{game_no_str}'
     #  そのディレクトリ無かったら作る
     if not os.path.exists(gameDir):
         os.mkdir(gameDir)
 
     index = url[-7:]
     #  HTMLファイルの保存
-    with open(gameDir + f'\\{index}.html', 'w', encoding='utf-8') as f:
+    with open(gameDir + r'\{index}.html', 'w', encoding='utf-8') as f:
         f.write(result.text)
         print('Done')
 
