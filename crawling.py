29f6f019d5615adf22955adb6c3146cfd35b4190
hundo <44696693+hundo303@users.noreply.github.com>
2020-12-02 22:12:53 +0900

    選手のプロファイルを取得できた

--- crawling.py
+++ crawling.py
@@ -33,21 +33,20 @@ def crawling_game_stats(year, opening_date='01-01', ending_date='12-31'):
 def fetch_day_stats(date_url_list):
     for date_url in date_url_list:
         for game_no in range(1, 7):
-            stats_url = date_url + str(game_no).zfill(2) + r'/stats'
-            file_dir = os.getcwd() + fr'\HTML_game_stats\{stats_url[-16:-6]}.html'
-
-            if os.path.exists(file_dir):
-                continue
-
-            time.sleep(1)
-            result = requests.get(stats_url)
-
-            if result.status_code == 404:
+            score_url = date_url + str(game_no).zfill(2) + '/score'
+            stats_url = date_url + str(game_no).zfill(2) + '/stats'
+            url_status = check_url_stats(score_url)
+            if url_status[0]:
                 break
-
-            with open(file_dir, 'w', encoding='utf-8') as f:
-                f.write(result.text)
-                print('Done')
+            elif url_status[1]:
+                continue
+            else:
+                time.sleep(0.5)
+                result = requests.get(stats_url)
+                save_dir = os.getcwd() + fr'\HTML_game_stats\{stats_url[-16:-6]}.html'
+                with open(save_dir, 'w', encoding='utf-8') as f:
+                    f.write(result.text)
+                    print('Done')
 
 
 #  URLの日付の部分まで作る
@@ -184,6 +183,13 @@ def judge_no_game(html):
         return False
 
 
+#  上関数のstats用
+def judge_no_game_stats(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    p = soup.select_one('#ing_brd > tbody > tr:nth-child(1) > td:nth-child(10) > p')
+    return p in None
+
+
 #  試合が2軍戦だとTrueを返す
 def judge_farm(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -194,6 +200,17 @@ def judge_farm(html):
         return False
 
 
+#  上関数のstats用
+def judge_farm_stats(html):
+    team_number_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '11', '12', '376']
+    soup = BeautifulSoup(html, 'html.parser')
+    #  print(soup)
+    table = soup.find('div', class_='bb-blowResultsTable')
+    table_team_number = table.table.get('class')[1].split('--')[1]
+
+    return not any([table_team_number == f'team{team_number}' for team_number in team_number_list])
+
+
 #  そのゲームのdirが存在したらTrueを返す
 def check_dir_exists(url):
     game_no_str = re.search('\d{10}', url).group()
@@ -201,12 +218,43 @@ def check_dir_exists(url):
     return os.path.exists(gameDir)
 
 
+def check_url_stats(url):
+    if check_dir_exists_stats(url):
+        return False, True
+
+    time.sleep(0.5)
+    result = requests.get(url)
+
+    if result.status_code == 404:
+        return True, False
+
+    elif judge_no_game(result.text):
+        return False, True
+
+    elif judge_farm(result.text):
+        return True, False
+
+    else:
+        return False, False
+
+
+def check_dir_exists_stats(url):
+    game_no_str = re.search('\d{10}', url).group()
+    gameDir = os.getcwd() + f'\\HTML_game_stats\\{game_no_str}.html'
+    return os.path.exists(gameDir)
+
+
+#  球団の選手リストを取ってくる
 def take_player_list():
     team_number_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '11', '12', '376']
     root_url = 'https://baseball.yahoo.co.jp/npb/teams/'
-    root_dir = os.getcwd() + '\\HTML_player'
 
     for team_number in team_number_list:
+        save_dir = fr'{os.getcwd()}\HTML_player\{team_number}'
+
+        if os.path.exists(save_dir):
+            continue
+
         team_url = root_url + team_number + '/memberlist'
         p_url = team_url + '?kind=p'
         b_url = team_url + '?kind=b'
@@ -215,10 +263,67 @@ def take_player_list():
         time.sleep(0.5)
         b_result = requests.get(b_url)
 
-        os.makedirs(fr'{root_dir}\{team_number}', exist_ok=True)
-        with open(fr'{root_dir}\{team_number}\P.html', 'w', encoding='utf-8') as f:
+        os.makedirs(save_dir, exist_ok=True)
+        with open(fr'{save_dir}\P.html', 'w', encoding='utf-8') as f:
             f.write(p_result.text)
             print('Done')
-        with open(fr'{root_dir}\{team_number}\B.html', 'w', encoding='utf-8') as f:
+        with open(fr'{save_dir}\B.html', 'w', encoding='utf-8') as f:
             f.write(b_result.text)
             print('Done')
+
+
+def take_player_profile():
+    take_player_list()
+    team_number_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '11', '12', '376']
+    player_profile_url_list = make_player_profile_url_list()
+
+    for team_number in team_number_list:
+        team_player_url_list = player_profile_url_list[team_number]
+        save_dir = rf'{os.getcwd()}\HTML_player\{team_number}'
+
+        for player_url in team_player_url_list:
+            player_id = player_url.split('/')[6]
+            player_file = save_dir+rf'\{player_id}.html'
+            if os.path.exists(player_file):
+                continue
+
+            time.sleep(0.5)
+            result = requests.get(player_url)
+            with open(player_file, 'w', encoding='utf-8') as f:
+                f.write(result.text)
+                print('Done')
+
+
+
+def make_player_profile_url_list():
+    team_number_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '11', '12', '376']
+    url_list_by_team = {}
+
+    for team_number in team_number_list:
+        team_url_list = []
+
+        pitcher_list_dir = fr'{os.getcwd()}\HTML_player\{team_number}\P.html'
+        batter_list_dir = fr'{os.getcwd()}\HTML_player\{team_number}\B.html'
+
+        with open(pitcher_list_dir, encoding='utf-8') as f:
+            team_url_list.extend(make_player_profile_url(f))
+
+        with open(batter_list_dir, encoding='utf-8') as f:
+            team_url_list.extend(make_player_profile_url(f))
+
+        url_list_by_team[team_number] = team_url_list
+
+    return url_list_by_team
+
+
+def make_player_profile_url(html):
+    url_list = []
+    root_url = 'https://baseball.yahoo.co.jp/'
+    soup = BeautifulSoup(html, 'html.parser')
+    tr_list = soup.select('#tm_plyr > tr')
+    for tr in tr_list:
+        td_list = tr.select('td')
+        player_profile_url = root_url + td_list[1].a.get('href')
+        url_list.append(player_profile_url)
+
+    return url_list
